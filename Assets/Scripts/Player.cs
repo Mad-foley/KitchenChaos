526b590ccde6ed57cb36cbc2708fee77621d74bf
Maddy Foley <maddyfoley5@gmail.com>
2024-02-26 12:12:19 -0500

    started unity tutorial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //attribute allows us to view in unity editor, private alone doesn't let us
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        //keypad movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        //prevents diagnoal path from moving faster
+        inputVector = inputVector.normalized;
+
+        //convert to 3D vector best way to write code
+        //watch necassary inputs and then convert if needed
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        //update player position
+        //multiply time so that it doesn't move every update but to time
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KitchenChaos
+tutorial practice
