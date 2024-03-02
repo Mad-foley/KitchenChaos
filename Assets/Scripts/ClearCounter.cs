d96dcc097d2f04c757b1d8e0d094ddbb5cae5ec3
Maddy Foley <maddyfoley5@gmail.com>
2024-03-02 14:43:22 -0500

    spawn tomato

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    //can use Transform or GameObject in this scenerio
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
     // Start is called before the first frame update
     public void Interact()
     {
         Debug.Log("lol");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
