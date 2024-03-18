c0f3bf2b3ec3fab1f27b2a28b5323697f754bd67
Maddy Foley <maddyfoley5@gmail.com>
2024-03-18 13:32:21 -0400

    added counter animation

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter
 {
+
+    public event EventHandler OnPlayerGrabbedObject;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player)
@@ -13,6 +16,7 @@ public class ContainerCounter : BaseCounter
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
         }
     }
 
---
+++ Assets/Scripts/ContainerCounterVisual.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class ContainerCounterVisual : MonoBehaviour
+{
+    private const string OPEN_CLOSE = "OpenClose";
+    [SerializeField] private ContainerCounter containerCounter;
+    private Animator animator;
+
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start(){
+        containerCounter.OnPlayerGrabbedObject += ContainerCounter_OnPLayerGrabbedObject;
+    }
+
+    private void ContainerCounter_OnPLayerGrabbedObject(object sender, System.EventArgs e) {
+        animator.SetTrigger(OPEN_CLOSE);
+    }
+}
