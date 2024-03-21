4a1bb3c424dfd53e1cd9052c36b2ef8763faae6f
Maddy Foley <maddyfoley5@gmail.com>
2024-03-21 11:32:08 -0400

    added cutting counter prefab

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -6,6 +6,20 @@ public class CuttingCounter : BaseCounter
 {
     public override void Interact(Player player)
     {
+         if (!HasKitchenObject()){
+            if(player.HasKitchenObject()){
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                //player has nothing
+            }
+        } else {
+            if(player.HasKitchenObject()){
 
+            } else {
+                //player not carrying anything
+                //give to player
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
