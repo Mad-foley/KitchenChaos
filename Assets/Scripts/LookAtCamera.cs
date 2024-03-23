2b3c0986df997718b2601de59e046af4b6774b58
Maddy Foley <maddyfoley5@gmail.com>
2024-03-23 16:54:54 -0400

    added more camera look at angles to camera script

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -8,6 +8,8 @@ public class LookAtCamera : MonoBehaviour
     private enum Mode {
         LookAt,
         LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
     }
 
     [SerializeField] private Mode mode;
@@ -22,7 +24,12 @@ public class LookAtCamera : MonoBehaviour
             Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
             transform.LookAt(transform.position + dirFromCamera);
             break;
+        case Mode.CameraForward:
+            transform.forward = Camera.main.transform.forward;
+            break;
+        case Mode.CameraForwardInverted:
+            transform.forward = -Camera.main.transform.forward;
+            break;
     }
-
    }
 }
